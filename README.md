59a5ed2b12c501350c632d6f1045ce9e43e55934
Anthony <bonsilaoanthony@gmail.com>
2022-08-21 22:34:45 -0400

    sql statements for db

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 This project (or rather example) is part of Chapter 9's 'CMS with OOP' and is from the book *PHP Advanced and Object-Oriented Programming* by Larry Ullman. 
+
+This project focuses on two important ascpets of a CMS:
+- Pages
+- Users
+
+Requisite Functionality:
+- Pages can be added.
+- The most recent pages are previewed on the home page.
+- An individual page can be  seen in its entirety.  
+- A user can log in.
+- A user can log out.
+- Only administrative users, or the author of the currently viewed page  of content, will be provided access to edit the page.
+- Only administrative users or author users can create new pages. 
+
+Users Table |-
+-|-
+**Column** | **Type**
+Id | INT 
+userType | ENUM
+username | VARCHAR(30)
+email | VARCHAR(40)
+pass | CHAR(40)
+
+
+Pages Table |-
+-|-
+**Column** | **Type**
+Id | INT 
+creatorId | INT
+title | VARCHAR(100)
+content | TEXT
+dateUpdated | TIMESTAMP
+dateAdded  | TIMESTAMP
---
+++ create.sql
@@ -0,0 +1,32 @@
+CREATE TABLE users (
+id INT UNISGNED NOT NULL AUTO_INCREMENT,
+userType ENUM('public', 'author', 'admin'),
+username VARCHAR(30) NOT NULL,
+email VARCHAR(40) NOT NULL,
+pass CHAR(40) NOT NULL,
+dateAdded TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+PRIMARY KEY (id),
+UNIQUE (username),
+UNIQUE (email),
+INDEX login (username, pass)
+);
+
+CREATE TABLE pages (
+id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+creatorId INT UNSIGNED NOT NULL,
+title VARCHAR(100) NOT NULL,
+content TEXT NOT NULL,
+dateUpdated  TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+dateAdded TIMESTAMP NOT NULL DEFAULT NOW(),
+PRIMARY KEY (id),
+INDEX (creatorId),
+INDEX (dateUpdated)
+);
+
+INSERT INTO users VALUES 
+(NULL, 'public', 'publicUser', 'public@example.com', SHA1('publicPass'), NULL),
+(NULL, 'author', 'authorUser', 'author@example.com', SHA1('authorPass'), NULL),
+(NULL, 'admin', 'adminUser', 'admin@example.com', SHA1('adminPass'), NULL);
+
+INSERT INTO pages VALUES 
+(NULL, 2, 'First Page', '<h1>Content Header</h2><p>This is the content paragraph</p>', NULL, NOW());
